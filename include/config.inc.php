2ef9fdc25ecd088510dcf50cc48327d70af969ea
kennethleebz <kenneth.lee.2013@sis.smu.edu.sg>
2015-02-28 17:03:35 +0800

    Added database library
    Database library for interfacing with the MySQL database

---
+++ include/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+//database server
+
+define('DB_SERVER', "localhost");
+define('DB_USER', "root");
+define('DB_PASS', "");
+define('DB_DATABASE', "cognitio");
+
+date_default_timezone_set('Asia/Singapore');
+
+
+?>
