7afe88696b82b5c0977769c06c70ee5414104fa5
kennethleebz <kenneth.lee.2013@sis.smu.edu.sg>
2015-03-05 06:31:48 +0800

    Amended crawlers
    Amended crawlers to include Singapore Oil Industry(Ezra, Ezion, Swiber)
    and Singapore banking industry (DBS,UOB,OCBC)

--- crawler.php
+++ crawler.php
@@ -11,32 +11,50 @@ $db = Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
 // connect to the server
 $db->connect();
 
+$stocks=array(
+	"D05.SI"=>"DBS Group",
+	"O39.SI"=>"OCBC Group",
+	"U11.SI"=>"UOB Group",
+	"AK3.SI"=>"Swiber",
+	"5DN.SI"=>"Ezra",
+	"5ME.SI"=>"Ezion"	
+);
+//Need to give priority
+//reuters.com
+//business
+//channelnewsasia
+//fxstreet
 //Get data
 
-$url = 'https://webhose.io/search?token=ae1f42ff-9e16-4e94-ad0b-fc603b85734e&format=json&q=%22DBS%20Group%22';
+foreach ($stocks as $key=>$value) {
+
+$url = 'https://webhose.io/search?token=ae1f42ff-9e16-4e94-ad0b-fc603b85734e&format=json&q=' . $value;
 $content = file_get_contents($url);
 $json = json_decode($content, true);
+	foreach($json['posts'] as $post) {
+		$record=$db->query_first("SELECT * FROM thread WHERE url ='".$post['thread']['url']."'");
+		//No record, so let's add into database
+		if (!isset($record['id'])) {
+			$data=array();
+			$data['symbol']=$key;
+			$data['url']=$post['thread']['url'];
+			$data['site_full']=$post['thread']['site_full'];
+			$data['site_section']=$post['thread']['site_section'];
+			$data['section_title']=$post['thread']['section_title'];
+			$data['title']=$post['thread']['title'];
+			$data['title_full']=$post['thread']['title_full'];
+			$data['published']=date("Y-m-d H:i:s",strtotime($post['thread']['published']));
+			$data['replies_count']=$post['thread']['replies_count'];
+			$data['participants_count']=$post['thread']['participants_count'];
+			$data['site_type']=$post['thread']['site_type'];
+			$data['country']=$post['thread']['country'];
+			$data['spam_score']=$post['thread']['spam_score'];
+			$data['main_image']=$post['thread']['main_image'];
+			$data['text']=$post['text'];
+			$data['importance']=rand(30,99) + round(abs(1-mt_rand()/mt_rand()),2);
+			$db->insert("thread",$data);
+		}
 
-foreach($json['posts'] as $post) {
-	$record=$db->query_first("SELECT * FROM thread WHERE url ='".$post['thread']['url']."'");
-	//No record, so let's add into database
-	if (!isset($record['id'])) {
-		$data=array();
-		$data['url']=$post['thread']['url'];
-		$data['site_full']=$post['thread']['site_full'];
-		$data['site_section']=$post['thread']['site_section'];
-		$data['section_title']=$post['thread']['section_title'];
-		$data['title']=$post['thread']['title'];
-		$data['title_full']=$post['thread']['title_full'];
-		$data['published']=date("Y-m-d H:i:s",strtotime($post['thread']['published']));
-        $data['replies_count']=$post['thread']['replies_count'];
-        $data['participants_count']=$post['thread']['participants_count'];
-        $data['site_type']=$post['thread']['site_type'];
-        $data['country']=$post['thread']['country'];
-        $data['spam_score']=$post['thread']['spam_score'];
-        $data['main_image']=$post['thread']['main_image'];
-		$data['text']=$post['text'];
-		$db->insert("thread",$data);
 	}
 }
 $db->close();
--- yahooStock.php
+++ yahooStock.php
@@ -10,25 +10,30 @@ $db = Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
 
 // connect to the server
 $db->connect();
-$stockSymbol="D05.SI";
-$url = 'http://ichart.finance.yahoo.com/table.csv?s=' . $stockSymbol;
-$content = file_get_contents($url);
-$your_array = explode("\n", $content);
-for ($i=1;$i<sizeof($your_array);$i++) {
-	$splitString=explode(",",$your_array[$i]);
-	$record=$db->query_first("SELECT * FROM prices WHERE symbol = '".$stockSymbol."' AND date ='".$splitString[0]."'");
-	if (!isset($record['id'])) {
-		$data=array();
-		$data['symbol']=$stockSymbol;
-		$data['date']=$splitString[0];
-		$data['open']=$splitString[1];
-		$data['high']=$splitString[2];
-		$data['low']=$splitString[3];
-		$data['close']=$splitString[4];
-		$data['volume']=$splitString[5];
-		$data['adjustedClose']=$splitString[6];
-		$data['updatedDatetime']=date("Y-m-d H:i:s");
-		$db->insert("prices",$data);
+//Retrieves stock prices for
+//SBI: DBS, UOB
+//SOI: Ezra, Ezion, Swiber
+$stockSymbols=array("D05.SI","O39.SI","U11.SI","AK3.SI","5DN.SI","5ME.SI");
+foreach ($stockSymbols as $stockSymbol) {
+	$url = 'http://ichart.finance.yahoo.com/table.csv?s=' . $stockSymbol;
+	$content = file_get_contents($url);
+	$your_array = min(explode("\n", $content),30);
+	for ($i=1;$i<sizeof($your_array);$i++) {
+		$splitString=explode(",",$your_array[$i]);
+		$record=$db->query_first("SELECT * FROM prices WHERE symbol = '".$stockSymbol."' AND date ='".$splitString[0]."'");
+		if (!isset($record['id'])) {
+			$data=array();
+			$data['symbol']=$stockSymbol;
+			$data['date']=$splitString[0];
+			$data['open']=$splitString[1];
+			$data['high']=$splitString[2];
+			$data['low']=$splitString[3];
+			$data['close']=$splitString[4];
+			$data['volume']=$splitString[5];
+			$data['adjustedClose']=$splitString[6];
+			$data['updatedDatetime']=date("Y-m-d H:i:s");
+			$db->insert("prices",$data);
+		}
 	}
 }
 $db->close();
