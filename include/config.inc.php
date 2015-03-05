d3081b8bee8ea830826a5cb1b085e1d4ffa65948
kennethleebz <kenneth.lee.2013@sis.smu.edu.sg>
2015-03-05 21:29:31 +0800

    Added if else switch for database connector
    Localhost then connect to root, non localhost connect to database

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,10 +1,19 @@
 <?php
 //database server
 
-define('DB_SERVER', "localhost");
-define('DB_USER', "root");
-define('DB_PASS', "");
-define('DB_DATABASE', "cognitio");
+if ($_SERVER['SERVER_NAME'] == "localhost")
+{
+	define('DB_SERVER', "localhost");
+	define('DB_USER', "root");
+	define('DB_PASS', "");
+	define('DB_DATABASE', "cognitio");
+} else {
+	define('DB_SERVER', "localhost");
+	define('DB_USER', "alphisco_cogniti");
+	define('DB_PASS', "kenneth87");
+	define('DB_DATABASE', "alphisco_cognitio");
+}
+
 
 date_default_timezone_set('Asia/Singapore');
 
