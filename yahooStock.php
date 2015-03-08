e7ca18a65588e33a083593166e022ac855028453
kennethleebz <kenneth.lee.2013@sis.smu.edu.sg>
2015-03-08 12:14:14 +0800

    Fixed bug
    Fixed bug that made yahoo crawler not work

--- yahooStock.php
+++ yahooStock.php
@@ -17,8 +17,8 @@ $stockSymbols=array("D05.SI","O39.SI","U11.SI","AK3.SI","5DN.SI","5ME.SI");
 foreach ($stockSymbols as $stockSymbol) {
 	$url = 'http://ichart.finance.yahoo.com/table.csv?s=' . $stockSymbol;
 	$content = file_get_contents($url);
-	$your_array = min(explode("\n", $content),30);
-	for ($i=1;$i<sizeof($your_array);$i++) {
+	$your_array = explode("\n", $content);
+	for ($i=1;$i<30;$i++) {
 		$splitString=explode(",",$your_array[$i]);
 		$record=$db->query_first("SELECT * FROM prices WHERE symbol = '".$stockSymbol."' AND date ='".$splitString[0]."'");
 		if (!isset($record['id'])) {
